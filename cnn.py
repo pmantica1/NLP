ddc6e42dc61563a042df252cf1fce935367efcf0
Pedro Matica <pmantica@mit.edu>
2017-11-26 19:59:53 -0500

    Implemented testing and development for CNN

--- cnn.py
+++ cnn.py
@@ -1,4 +1,5 @@
 import torch
+import numpy as np
 from torch import nn
 from torch.autograd import Variable
 from database import QueryDatabase
@@ -11,6 +12,10 @@ SIM_TITLE_VEC = "sim_title_vec"
 SIM_BODY_VEC = 'sim_body_vec'
 RAND_TITLE_VECS = "rand_title_vecs"
 RAND_BODY_VECS = "rand_body_vecs"
+SIMILARITY_VEC = "similarity_vec"
+BM25_SCORES = "bm25_scores"
+CAND_TITLE_VECS = "cand_title_vecs"
+CAND_BODY_VECS = "cand_body_vecs"
 
 class CNN(nn.Module):
     def __init__(self, feature_vector_dimensions, output_size, kernel_size):
@@ -44,7 +49,7 @@ class Loss(nn.Module):
         other_questions_batch = torch.cat([similar_question_batch, negative_questions_batch], 2)
         expanded_question_batch = question_batch.expand(other_questions_batch.data.shape)
         scores = self.cosine_similarity(expanded_question_batch, other_questions_batch)
-        margin = 0.1 * torch.ones(scores[:, 0].data.shape)
+        margin = 0.01 * torch.ones(scores[:, 0].data.shape)
         margin[0] = 0
         margin = Variable(margin)
         return (scores - (scores[:, 0] - margin).unsqueeze(1)).max(1)[0].mean()
@@ -54,8 +59,12 @@ def train(cnn, dataset, learning_rate, l2_weight_decay, n_epochs, batch_size):
     optimizer = torch.optim.Adam(cnn.parameters(), lr=learning_rate, weight_decay=l2_weight_decay)
     for epoch in xrange(n_epochs):
         data_loader = data.DataLoader(dataset, batch_size=batch_size, shuffle=True, drop_last=True)
+        counter = 0
         for batch in tqdm(data_loader):
+            counter += 1
             train_step(cnn, batch, optimizer)
+            if counter == 20:
+                break
 
 
 def train_step(cnn, batch, optimizer):
@@ -75,7 +84,7 @@ def train_step(cnn, batch, optimizer):
 
         questions_batch = evaluate(cnn, questions_title_batch, questions_body_batch)
         similar_questions_batch = evaluate(cnn, similar_questions_title_batch, similar_questions_body_batch)
-        negative_questions_batch = evaluate_negative_questions(cnn, negative_questions_title_batch, negative_questions_body_batch)
+        negative_questions_batch = evaluate_multi_questions(cnn, negative_questions_title_batch, negative_questions_body_batch)
 
         loss = loss_fn(questions_batch, similar_questions_batch, negative_questions_batch)
 
@@ -85,6 +94,59 @@ def train_step(cnn, batch, optimizer):
 
     optimizer.step(closure)
 
+def test(cnn, dataset, threshold):
+    data_loader = data.DataLoader(dataset, batch_size=1)
+    true_positives_list= []
+    true_negatives_list = []
+    false_positives_list = []
+    false_negatives_list = []
+    for batch in data_loader:
+        true_positives, true_negatives, false_positives, false_negatives = test_step(cnn, batch, threshold)
+        true_positives_list.append(true_positives)
+        true_negatives_list.append(true_negatives)
+        false_positives_list.append(false_positives)
+        false_negatives_list.append(false_negatives)
+
+    compute_metrics(true_positives_list, true_negatives_list, false_positives_list, false_negatives_list)
+
+def compute_metrics(true_positives_list, true_negatives_list, false_positives_list, false_negatives_list):
+    n = len(true_positives_list)
+
+    map = np.array([true_positives_list[i]/float(true_negatives_list[i] +true_positives_list[i]) for i in range(n)])
+    map = np.average(map[~np.isnan(map)])
+    print("The MAP score is "+str(map))
+    mar = np.average([true_positives_list[i] / float(false_positives_list[i] + true_positives_list[i]) for i in range(n)])
+    print("The MAR score is " + str(mar))
+
+
+def test_step(cnn, batch, threshold):
+    questions_title_batch = batch[TITLE_VEC]
+    questions_body_batch = batch[BODY_VEC]
+
+    candidate_questions_title_batch = batch[CAND_TITLE_VECS]
+    candidate_questions_body_batch = batch[CAND_BODY_VECS]
+
+    similarity_vector_batch = batch[SIMILARITY_VEC].numpy()
+
+    question_vector_batch = evaluate(cnn, questions_title_batch, questions_body_batch).data.numpy()
+    candidate_vector_batch = evaluate_multi_questions(cnn, candidate_questions_title_batch, candidate_questions_body_batch).data.numpy()
+
+    question_vec = question_vector_batch[0]
+    similarity_vector = similarity_vector_batch[0]
+    candidate_questions_vec  = candidate_vector_batch[0]
+    cosines = [np.cos(question_vec.flatten().dot(candidate_questions_vec[0].flatten())) for i in range(len(candidate_questions_vec))]
+    choices = np.array([1 if cos > threshold else 0 for cos in cosines])
+    true_positives =  choices.dot(similarity_vector)
+    true_negatives = (1-choices).dot(1-similarity_vector)
+    false_positives = (choices).dot(1-similarity_vector)
+    false_negatives = (1-choices).dot(similarity_vector)
+
+    return true_positives, true_negatives, false_positives, false_negatives
+
+
+
+
+
 
 def evaluate(cnn, title, body):
     title_vec = cnn(Variable(title.permute(0,2,1)))
@@ -92,7 +154,7 @@ def evaluate(cnn, title, body):
     return (title_vec + body_vec) / 2
 
 
-def evaluate_negative_questions(cnn, titles, bodies):
+def evaluate_multi_questions(cnn, titles, bodies):
     if len(titles[0]) != len(bodies[0]):
         raise RuntimeError("titles and bodies have different batch size")
     vectors = [evaluate(cnn, titles[:,i], bodies[:,i]) for i in xrange(len(titles[0]))]
@@ -109,14 +171,13 @@ if __name__ == "__main__":
     print loss
     loss.backward()
     """
-
     feature_vector_dimensions = 200
-    questions_vector_dimensions = 20
+    questions_vector_dimensions = 100
     kernel_size = 3
 
     learning_rate = 1e-3
     l2_weight_decay = 1e-5
-    n_epochs = 2
+    n_epochs = 1
     batch_size = 50
 
     cnn = CNN(feature_vector_dimensions, questions_vector_dimensions, kernel_size)
@@ -125,3 +186,7 @@ if __name__ == "__main__":
     dataset = database.get_training_dataset()
 
     train(cnn, dataset, learning_rate, l2_weight_decay, n_epochs, batch_size)
+
+    cnn = CNN(200, 20, 3)
+    dataset = database.get_validation_dataset()
+    test(cnn, dataset,0.5)
