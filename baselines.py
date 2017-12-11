d0e5b739fb6cb9487dc7caa5fa962b6859ce78d3
Daniel <danifili96@gmail.com>
2017-12-10 19:00:46 -0500

    added baseline model for part 2

--- baselines.py
+++ baselines.py
@@ -1,8 +1,12 @@
-from database import QueryDatabase
+from database import QueryDatabase, UbuntuDatabase, AndroidDatabase
 import metrics
 import pprint
+from sklearn.metrics.pairwise import paired_cosine_distances as cosine
+from tqdm import tqdm
+import torch
+from torch.utils import data
 
-def compute_baselines():
+def compute_baselines_part1():
     query_database = QueryDatabase()
     validation_set = query_database.get_validation_dataset()
     testing_set = query_database.get_testing_dataset()
@@ -17,6 +21,28 @@ def compute_baselines():
         metrics_list.append(metrics.compute_metrics(bm25_scores_list, similarity_vector_list))
     return {"validation": metrics_list[0], "testing": metrics_list[1]}
 
+
+def compute_baselines_part2():
+    android_database = AndroidDatabase(use_count_vectorizer=True)
+    validation_set = android_database.get_validation_dataset()
+    testing_set = android_database.get_testing_dataset()
+
+    metrics_list = []
+
+    for dataset in (validation_set, testing_set):
+        meter = metrics.AUCMeter()
+        for query_pair in tqdm(dataset):
+            question1_vec = (query_pair["id_1_title_vec"] + query_pair["id_1_body_vec"]) / 2.0
+            question2_vec = (query_pair["id_2_title_vec"] + query_pair["id_2_body_vec"]) / 2.0
+            score = (1 - cosine(question1_vec.numpy(), question2_vec.numpy()))[0]
+            similarity = query_pair["similarity"]
+
+            meter.add(torch.FloatTensor([score]), torch.LongTensor([similarity]))
+        metrics_list.append(meter.value(0.05))
+
+    return {"validation": metrics_list[0], "testing": metrics_list[1]}
+
+
 if __name__ == "__main__":
     pp = pprint.PrettyPrinter(indent=4)
-    pp.pprint(compute_baselines())
+    pp.pprint(compute_baselines_part2())
