44e504dd4ed39339b80153b1a9140163660127c1
Pedro Mantica <pmantica@mit.edu>
2017-12-17 03:50:23 +0000

    filled gaps in get_cnn_scores

--- get_cnn_scores.py
+++ get_cnn_scores.py
@@ -2,6 +2,7 @@ import pickle
 from nn_utils import test_auc_step 
 from database import AndroidDatabase 
 from torch import data 
+from load_model import load_cnn_encoder
 
 def compute_scores_and_similarities(model, dataset):
     data_loader = data.DataLoader(dataset, batch_size=1, shuffle=False)
@@ -17,9 +18,15 @@ if __name__ == "__main__":
 	android_database = database.AndroidDatabase(use_glove=True)
     validation_set = android_database.get_validation_dataset()
     testing_set = android_database.get_testing_dataset()
-    #LOAD MODEL HERE 
-    validation_scores, validation_similarities = compute_scores_and_similarities(# ADD MODEL HERE, validation_set)
-    testing_scores, testing_similarities = compute_scores_and_similarities(# ADD MODEL HERE, testing_set)
+    
+    filepath = "cnn_encoder.pt"
+    feature_vector_dimensions = 300
+    questions_vector_dimensions = 500
+    kernel_size = 3
+    encoder = load_cnn_encoder(filepath, feature_vector_dimensions, questions_vector_dimensions, kernel_size)
+
+    validation_scores, validation_similarities = compute_scores_and_similarities(encoder, validation_set)
+    testing_scores, testing_similarities = compute_scores_and_similarities(encoder, testing_set)
     pickle.dump(validation_scores, open("validation_vectorizer_scores.pkl", "wb"))
     pickle.dump(validation_similarities, open("validation_vectorizer_similarities.pkl", "wb"))
     pickle.dump(testing_scores, open("testing_vectorizer_scores.pkl", "wb"))
---
+++ load_model.py
@@ -0,0 +1,25 @@
+from database import TransferLearningDatabase
+from cnn import CNN
+from nn_utils import test_auc
+import torch
+
+
+def load_cnn_encoder(filepath, feature_vector_dimensions, questions_vector_dimensions, kernel_size):
+	encoder = CNN(feature_vector_dimensions, questions_vector_dimensions, kernel_size).cuda()
+	encoder.load_state_dict(torch.load(filepath))
+	return encoder
+
+
+if __name__ == "__main__":
+	database = TransferLearningDatabase()
+	test_set = database.get_testing_set()
+
+	filepath = "cnn_encoder.pt"
+	feature_vector_dimensions = 300
+	questions_vector_dimensions = 500
+	kernel_size = 3
+
+	encoder = CNN(feature_vector_dimensions, questions_vector_dimensions, kernel_size).cuda()
+	encoder.load_state_dict(torch.load(filepath))
+
+	print test_auc(encoder, test_set)
