abc0884eb4e1513b020ebd7a3c51b57e605f9b0f
Pedro Matica <pmantica@mit.edu>
2017-12-17 01:47:50 -0500

    Added a way to combine tf-idf with nn

---
+++ scores_combiner.py
@@ -0,0 +1,6 @@
+import pickle 
+
+pickle.load(open("validation_vectorizer_scores.pkl", "rb"))
+pickle.load(open("validation_vectorizer_similarities.pkl", "rb"))
+pickle.load(open("testing_vectorizer_scores.pkl", "rb"))
+pickle.load(open("testing_vectorizer_similarities.pkl", "rb"))
